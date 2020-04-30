dea60ee906ea3f95511d91125956512a4cb65116
QA engineer <kotarakh@gmail.com>
2020-04-30 05:42:27 +0300

    constClass

---
+++ src/test/java/Page.java
@@ -0,0 +1,11 @@
+
+public class Page {
+
+    public int page;
+
+    @Override
+    public String toString() {
+        return String.valueOf(page);
+    }
+    //получаем номер page и из json парсим в строку
+}
