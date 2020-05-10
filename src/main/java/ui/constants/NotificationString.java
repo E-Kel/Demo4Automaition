8649386b7874b4b5cd4d9817ede328c2f00a46f5
Biskota <dbiskota@gmail.com>
2020-05-10 21:28:55 +0300

    Added constant with expected notification

--- src/main/java/ui/constants/NotificationString.java
+++
@@ -1,5 +0,0 @@
-package ui.constants;
-
-public class NotificationString {
-    public static final String EMAIL_RECOVERY_NOTIFICATION = "A confirmation email has been sent to your address: ";
-}
--- src/main/java/ui/login/LoginPage.java
+++ src/main/java/ui/login/LoginPage.java
@@ -5,22 +5,21 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
-import static ui.constants.NotificationString.EMAIL_RECOVERY_NOTIFICATION;
 
 public class LoginPage {
-    private SelenideElement emailElement = $(By.name("email"));
-    private SelenideElement passwordElement = $(By.name("passwd"));
+    private SelenideElement emailEl = $(By.name("email"));
+    private SelenideElement passEl = $(By.name("passwd"));
     private SelenideElement forgotLink = $(By.linkText("Forgot your password?"));
     private SelenideElement successAlert = $(".alert.alert-success");
 
-    public void setCredentials(String email, String pass) {
-        emailElement.setValue(email);
-        passwordElement.setValue(pass).pressEnter();
+    public void credentials(String email, String pass) {
+        emailEl.setValue(email);
+        passEl.setValue(pass).pressEnter();
     }
 
-    public void recoverPassword(String email) {
+    public void recoveryPassword(String email) {
         forgotLink.click();
-        emailElement.setValue(email).pressEnter();
-        successAlert.shouldHave(Condition.textCaseSensitive(EMAIL_RECOVERY_NOTIFICATION + email));
+        emailEl.setValue(email).pressEnter();
+        successAlert.shouldHave(Condition.textCaseSensitive("A confirmation email has been sent to your address: " + email));
     }
 }
