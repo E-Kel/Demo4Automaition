5adf95dc10b244eecb5db3f4eded5f0b48bc57f2
Biskota <dbiskota@gmail.com>
2020-05-10 21:26:01 +0300

    Added constants with page addresses

--- src/main/java/ui/constants/URL.java
+++ src/main/java/ui/constants/URL.java
@@ -5,4 +5,6 @@ public class URL {
     }
 
     public static final String BASE_URL = "http://automationpractice.com/index.php";
+    public static final String AUTHENTICATION_URL = BASE_URL + "?controller=authentication";
+    public static final String MY_ACCOUNT_URL = BASE_URL + "?controller=my-account";
 }
