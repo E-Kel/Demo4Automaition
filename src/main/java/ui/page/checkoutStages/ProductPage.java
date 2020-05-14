7b3358ed096c4ae35cc9a637e5732a43b1bb2f86
QA engineer <kotarakh@gmail.com>
2020-05-14 12:38:50 +0300

    revert waiters

--- src/main/java/ui/page/checkoutStages/ProductPage.java
+++ src/main/java/ui/page/checkoutStages/ProductPage.java
@@ -4,19 +4,16 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.byId;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProductPage {
-    private SelenideElement addToCartButton = $(byId("add_to_cart")).$(withText("Add to cart"));
-    private SelenideElement closePopupIcon = $(By.xpath("//span[@class='cross']"));
-    private SelenideElement cartLable = $(By.cssSelector(".shopping_cart > a"));
+    private SelenideElement addToCartButton = $(byId("add_to_cart"));
+    private SelenideElement closePopupIcon = $(byXpath("//span[@class='cross']"));
+    private SelenideElement cartLable = $(byCssSelector(".shopping_cart > a"));
 
 
     public void clickAddToCartButton() {
-
-
         addToCartButton.waitUntil(Condition.visible, 10000).click();
     }
 
